25b2b731e22885449de6fae1ce6c2c75e91f69de
bluelong <bluelong001@outlook.com>
2019-03-24 07:58:50 +0800

    init

---
+++ src/main/java/me/study/silang/SilangApplication.java
@@ -0,0 +1,13 @@
+package me.study.silang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilangApplication.class, args);
+    }
+
+}
