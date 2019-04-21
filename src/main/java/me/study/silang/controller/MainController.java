af8b573b2cc3f8f41013f1e9a9d336a7cf054c03
bluelong <bluelong001@outlook.com>
2019-04-21 15:55:40 +0800

    update

--- src/main/java/me/study/silang/controller/MainController.java
+++ src/main/java/me/study/silang/controller/MainController.java
@@ -18,9 +18,17 @@ public class MainController {
     private IUserService userService;
 
     @PostMapping("/login")
-    public Rest login(String username,String password){
+    public Rest clientLogin(String username,String password){
         User user =userService.getOne(new QueryWrapper<User>().lambda().eq(User::getPassword,password).eq(User::getUsername,username));
         if(user==null)return Rest.fail("认证失败");
+        return Rest.ok().data(TokenUtils.makeToken(user));
+    }
+
+    @PostMapping("/back/login")
+    public Rest webAdminLogin(String username,String password){
+        User user =userService.getOne(new QueryWrapper<User>().lambda().eq(User::getPassword,password).eq(User::getUsername,username));
+        if(user==null)return Rest.fail("认证失败");
+        if(user.getRole()!=2)return Rest.fail("你不是管理员，禁止登录");
         return Rest.ok().data(TokenUtils.makeToken(user));
     }
 
